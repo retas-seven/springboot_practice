b40244f64d30d257e6b956b848edad5316044e1a
t-osawa <retas_seven@hotmail.co.jp>
2018-03-14 02:13:42 +0900

    SpringBootプロジェクトを作成

---
+++ src/main/java/com/tuyano/springboot/MyBootGAppApplication.java
@@ -0,0 +1,12 @@
+package com.tuyano.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBootGAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBootGAppApplication.class, args);
+	}
+}
