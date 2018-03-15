ff0fe42d3c92804050b9896a38b8c5ee49bb8bce
t-osawa <retas_seven@hotmail.co.jp>
2018-03-16 01:53:54 +0900

    アドレス登録機能専用のコントローラーを作成した

--- src/main/java/com/tuyano/springboot/HelloController.java
+++ src/main/java/com/tuyano/springboot/HelloController.java
@@ -1,16 +1,11 @@
 package com.tuyano.springboot;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.tuyano.springboot.form.InputAddressForm;
-
 @Controller
-@SessionAttributes(value = {"inputAddressForm"})
 public class HelloController {
 
 	@RequestMapping(value="/", method=RequestMethod.GET)
@@ -23,27 +18,5 @@ public class HelloController {
 	public ModelAndView send(ModelAndView mav) {
 		mav.setViewName("input_address");
 		return mav;
-	}
-
-	@RequestMapping(value="/input_address", method=RequestMethod.POST)
-	public String registAddress(InputAddressForm inputAddressForm, Model model) {
-		System.out.println(inputAddressForm);
-		model.addAttribute("inputAddressForm", inputAddressForm);
-		return "confirm_address";
-	}
-	
-	@RequestMapping(value="/confirm_address", method=RequestMethod.POST)
-	public String confirmAddress(InputAddressForm inputAddressForm, Model model) {
-		System.out.println(inputAddressForm);
-		
-		// ここにDB登録処理を追加する
-		
-		return "complete_address";
-	}
-	
-	@RequestMapping(value="/complete_address", method=RequestMethod.POST)
-	public String completeAddress() {
-		// ここで後処理をする
-		return "index";
 	}
 }
