174bb269ddfa743836023f17dcdca6a10b60f098
t-osawa <retas_seven@hotmail.co.jp>
2018-04-09 03:25:42 +0900

    Formのセッション保持を@ModelAttributeを付加したメソッドで行うことにした

--- src/main/java/com/tuyano/springboot/controller/regist_address/RegistAddressController.java
+++ src/main/java/com/tuyano/springboot/controller/regist_address/RegistAddressController.java
@@ -2,10 +2,13 @@ package com.tuyano.springboot.controller.regist_address;
 
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
@@ -18,13 +21,19 @@ import com.tuyano.springboot.service.regist_address.RegistAddressService;
 @SessionAttributes(value = {"inputAddressForm"})
 @RequestMapping("/regist_address")
 public class RegistAddressController {
+	private static final Logger log = LoggerFactory.getLogger(RegistAddressController.class);
 
 	@Autowired
 	RegistAddressService registAddressService;
 	
+	@ModelAttribute(value = "inputAddressForm")
+	public InputAddressForm setUpInputAddressForm() {
+		log.info("アドレス登録機能用のFormを初期化");
+	    return new InputAddressForm();
+	}
+	
 	@RequestMapping(value="/init")
 	public String send(Model model) {
-		model.addAttribute("inputAddressForm", new InputAddressForm());
 		return "/regist_address/input";
 	}
 	
