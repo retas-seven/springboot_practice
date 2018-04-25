4d69c71b63d713e9c2a942ec4b669d0615354f26
t-osawa <retas_seven@hotmail.co.jp>
2018-04-25 23:10:39 +0900

    メール送信はFreemarkerを使用するように変更した

--- src/main/java/com/tuyano/springboot/util/MailUtil.java
+++ src/main/java/com/tuyano/springboot/util/MailUtil.java
@@ -1,30 +1,63 @@
 package com.tuyano.springboot.util;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.mail.MailSender;
-import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
+
+import com.tuyano.springboot.exceptioon.SystemException;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 
 @Component
 @PropertySource("classpath:/mail.properties")
 public class MailUtil {
 	private static final Logger log = LoggerFactory.getLogger(MailUtil.class);
 	
-	@Autowired
-	private MailSender mailSender;
+    @Autowired
+    private JavaMailSender emailSender;
+
+    @Autowired
+    private Configuration freemarkerConfig;
+    
+    @Async
+    public void send(String sendToAddr, String url) {
+    	try {
+	        MimeMessage message = emailSender.createMimeMessage();
+	        MimeMessageHelper helper = new MimeMessageHelper(
+	        		message
+	                , MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED
+	                , StandardCharsets.UTF_8.name());
 	
-	@Async
-    public void send(String sendToAddr, String content) {
-		log.info("メール送信処理：開始");
-        SimpleMailMessage msg = new SimpleMailMessage();
-        msg.setTo(sendToAddr);
-        msg.setSubject("WebAdressbook登録確認");
-        msg.setText("＜URL＞\n" + content);
-        mailSender.send(msg);
-		log.info("メール送信処理：終了");
+	        Map<String, String> model = new HashMap<>();
+	        model.put("url", url);
+	        
+	        Template t = freemarkerConfig.getTemplate("mail/regist_address.ftl");
+	        String html = FreeMarkerTemplateUtils.processTemplateIntoString(t, model);
+	
+	        helper.setTo(sendToAddr);
+	        helper.setText(html, true);
+	        helper.setSubject("Web Adressbook 登録受付確認");
+	        helper.setFrom("no-reply@webaddressbook.com");
+	
+	        emailSender.send(message);
+    	} catch (MessagingException | IOException | TemplateException e) {
+    		throw new SystemException(e);
+    	}
     }
 }
---
+++ src/main/resources/templates/mail/regist_address.ftl
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+    <head>
+        <meta charset="utf-8">
+        <title>WEB Addressbook</title>
+    </head>
+    
+    <body>
+        <p>URL</p>
+        <a href="${url}">登録完了手続リンク</a>
+    </body>
+</html>
