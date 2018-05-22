5a9d7bc3e3dab32e0104450aa70ff5bfa142738c
t-osawa <retas_seven@hotmail.co.jp>
2018-05-23 00:02:45 +0900

    静的ドキュメントを追加

--- src/main/java/com/tuyano/springboot/security/WebSecurityConfig.java
+++ src/main/java/com/tuyano/springboot/security/WebSecurityConfig.java
@@ -27,7 +27,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         web.ignoring().antMatchers(
                             "/img/**",
                             "/css/**",
-                            "/js/**"
+                            "/js/**",
+                            "/doc/**"
                             );
     }
 
