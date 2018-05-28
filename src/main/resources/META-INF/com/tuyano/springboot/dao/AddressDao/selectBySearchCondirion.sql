5af59b6e8bb5d2a0a99c1122eb283504bf9a8da3
t-osawa <retas_seven@hotmail.co.jp>
2018-05-29 00:00:37 +0900

    電話番号、メールアドレスでも検索できるようにした

--- src/main/resources/META-INF/com/tuyano/springboot/dao/AddressDao/selectBySearchCondirion.sql
+++ src/main/resources/META-INF/com/tuyano/springboot/dao/AddressDao/selectBySearchCondirion.sql
@@ -5,10 +5,13 @@ from
 where
      BELONG_USER_EMAIL = /* belongUserEmail */1
 and (
-    LASTNAME_KANA  like CONCAT(/* searchCondition */'ア', '%')
-or  FIRSTNAME_KANA like CONCAT(/* searchCondition */'ア', '%')
-or  LASTNAME       like CONCAT(/* searchCondition */'ア', '%')
-or  FIRSTNAME      like CONCAT(/* searchCondition */'ア', '%')
+    LASTNAME_KANA       like CONCAT(/* searchCondition */'ア', '%')
+or  FIRSTNAME_KANA      like CONCAT(/* searchCondition */'ア', '%')
+or  LASTNAME            like CONCAT(/* searchCondition */'ア', '%')
+or  FIRSTNAME           like CONCAT(/* searchCondition */'ア', '%')
+or  HOME_PHONE_NUMBER   like CONCAT(/* searchCondition */'ア', '%')
+or  MOBILE_PHONE_NUMBER like CONCAT(/* searchCondition */'ア', '%')
+or  EMAIL               like CONCAT(/* searchCondition */'ア', '%')
 )
 order by
     CONCAT(LASTNAME_KANA, FIRSTNAME_KANA, LASTNAME, FIRSTNAME)
