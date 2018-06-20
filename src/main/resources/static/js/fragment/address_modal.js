dad85edde909dab0632e392bbc707357899ec2da
t-osawa <retas_seven@hotmail.co.jp>
2018-06-21 04:22:50 +0900

    アドレス編集時に右上のアイコンを切り替えるようにした

--- src/main/resources/static/js/fragment/address_modal.js
+++ src/main/resources/static/js/fragment/address_modal.js
@@ -1,11 +1,20 @@
 $(function() {
+	
+	/**
+	 * 詳細画面の編集アイコンをクリックしたときの処理
+	 */
     $('#name_toggle').click(function(e) {
         if ($('#name_output').hasClass('name_is_hide')) {
+        	//---------------------------------
         	// 切り替え後のモードが「表示」の場合
-            var lastnameKana = $('#modalLastnameKana').val();
+            //---------------------------------
+        	var lastnameKana = $('#modalLastnameKana').val();
             var firstnameKana = $('#modalFirstnameKana').val();
             var lastname = $('#modalLastname').val();
             var firstname = $('#modalFirstname').val();
+            
+            // アイコン切り替え
+        	$('#name_toggle > i').removeClass('fa-list-alt').addClass('fa-edit');
             
             $('#modalFullname').text(lastname + ' ' + firstname);
             $('#modalFullnameKana').text('【' + lastnameKana + ' ' + firstnameKana + '】');
@@ -25,7 +34,12 @@ $(function() {
             $('#modalAddress').removeClass('input_mode').attr('readonly',true);
             $('#modalBuildingName').removeClass('input_mode').attr('readonly',true);
         } else {
+        	//---------------------------------
         	// 切り替え後のモードが「入力」の場合
+        	//---------------------------------
+        	// アイコン切り替え
+            $('#name_toggle > i').removeClass('fa-edit').addClass('fa-list-alt');
+        	
 //        	$('#name_input_output').addClass('address_modal_header_nobiru');
             $('#modalFullname').addClass('input_mode').attr('readonly',false);
             $('#modalFullnameKana').addClass('input_mode').attr('readonly',false);
